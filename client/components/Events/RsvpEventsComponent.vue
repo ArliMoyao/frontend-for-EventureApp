f072060916e654c666ba6c1e2dc1e79fd6e76975
Arli Moyao-Ramirez <104865433+ArliMoyao@users.noreply.github.com>
2024-11-02 23:36:46 -0400

    fix

--- client/components/Events/RsvpEventsComponent.vue
+++ client/components/Events/RsvpEventsComponent.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useEventStore } from "@/stores/eventStore";
 import { useUserStore } from "@/stores/user";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import { storeToRefs } from "pinia";
 
 const eventStore = useEventStore();
 const userStore = useUserStore();
 const { events, rsvpEvents, loading, error, fetchRsvpEvents } = eventStore;
-const { currentUser } = userStore;
+const { currentUsername } = storeToRefs(useUserStore());
 
 onMounted(fetchRsvpEvents);
 
@@ -17,17 +18,13 @@ onMounted(fetchRsvpEvents);
 
 <template>
   <div>
-    <h1>RSVP'd Events for {{ currentUser }}</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">{{ error }}</div>
-    <div v-else>
-      <div v-for="event in rsvpEvents" :key="event._id.toString()" class="event-post">
-        <h2>{{ event.title }}</h2>
-        <p>{{ event.description }}</p>
-        <p><strong>Date:</strong> {{ new Date(event.date).toLocaleDateString() }}</p>
-        <p><strong>Location:</strong> {{ event.location }}</p>
-        <p><strong>Capacity:</strong> {{ event.capacity }}</p>
-      </div>
+    <h1>RSVP'd Events for {{ currentUsername }}</h1>
+    <div v-for="event in rsvpEvents" :key="event._id.toString()" class="event-post">
+      <h2>{{ event.title }}</h2>
+      <p>{{ event.description }}</p>
+      <p><strong>Date:</strong> {{ new Date(event.date).toLocaleDateString() }}</p>
+      <p><strong>Location:</strong> {{ event.location }}</p>
+      <p><strong>Capacity:</strong> {{ event.capacity }}</p>
     </div>
   </div>
 </template>
